f4447f0e7eba46e996e76d34ab7843220489cf1d
Nickolas Burr <nickolasburr@gmail.com>
2020-02-17 20:52:34 -0500

    Initial commit

---
+++ registration.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * registration.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simpleredirects/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleRedirects
+ * @copyright     Copyright (C) 2020 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'AuroraExtensions_SimpleRedirects',
+    __DIR__
+);
